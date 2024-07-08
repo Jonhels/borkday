07a9f3000c7e24c896fe22163d87243d9f081d6b
Jonhels <jonhels8888@gmail.com>
2024-07-08 15:27:05 +0200

    updated errors in skip

--- myapp/commands/utility/skip.js
+++ myapp/commands/utility/skip.js
@@ -14,11 +14,13 @@ module.exports = {
   async execute(interaction) {
     const now = Date.now();
     if (now - lastSkipTime < skipCooldown) {
-      await interaction.followUp("Please wait before skipping again.");
+      await interaction.reply("Please wait before skipping again.");
+      logger.info("Skip command cooldown triggered.");
       return;
     }
     lastSkipTime = now;
     await interaction.deferReply();
+    logger.info("Processing skip command.");
 
     const guildId = interaction.guild.id;
     const connection = getVoiceConnection(guildId);
@@ -27,12 +29,11 @@ module.exports = {
       await interaction.followUp(
         "Barkbark 🐶 I am not connected to any voice channel.",
       );
+      logger.error("No voice connection found.");
       return;
     }
 
-    // Get the voice channel of the member who triggered the command
     const userVoiceChannel = interaction.member.voice.channel;
-
     if (
       !userVoiceChannel ||
       userVoiceChannel.id !== connection.joinConfig.channelId
@@ -40,34 +41,40 @@ module.exports = {
       await interaction.followUp(
         "Barkbark 🐶 You need to be in the same voice channel to skip songs.",
       );
+      logger.error("User not in the correct voice channel.");
       return;
     }
 
     const player = players.get(guildId);
-    const currentQueue = queue.get(guildId);
-
-    if (!currentQueue || currentQueue.length === 0) {
-      await interaction.followUp("Barkbark 🐶 There are no songs to skip.");
+    if (!player) {
+      await interaction.followUp("Barkbark 🐶 No player found.");
+      logger.error("Player not found for guild.");
       return;
     }
 
-    // Skip the current song
-    player.stop();
+    const currentQueue = queue.get(guildId);
+    if (!currentQueue || currentQueue.length === 0) {
+      await interaction.followUp("Barkbark 🐶 There are no songs to skip.");
+      logger.error("No songs in queue to skip.");
+      return;
+    }
 
-    // Remove the current song from the queue
+    player.stop();
     currentQueue.shift();
+    logger.info("Song skipped.");
 
     if (currentQueue.length > 0) {
       const nextSongUrl = currentQueue[0];
       playSong(guildId, interaction, nextSongUrl);
-      // Inform about the number of songs left after skipping
       await interaction.followUp(
         `Skipped! 🎵 ${currentQueue.length} song(s) left in the queue.`,
       );
+      logger.info("Next song started.");
     } else {
       await interaction.followUp(
         "Barkbark 🐶 No more songs in the queue. The queue is now empty.",
       );
+      logger.info("Queue is now empty.");
     }
   },
 };
